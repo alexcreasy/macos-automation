84249b1c90c928a4dd1a61126d2fa4e6440bf4c0
Alex Creasy <acreasy@redhat.com>
2024-01-14 23:25:56 +0000

    inc

---
+++ roles/zsh/files/shell/01-xdg.zsh
@@ -0,0 +1,18 @@
+# The $XDG_DATA_HOME environment variable is not set, make sure to add it to your shell's configuration before setting any of the other environment variables!
+#     ⤷ The recommended value is: $HOME/.local/share
+
+# The $XDG_CONFIG_HOME environment variable is not set, make sure to add it to your shell's configuration before setting any of the other environment variables!
+#     ⤷ The recommended value is: $HOME/.config
+# The $XDG_STATE_HOME environment variable is not set, make sure to add it to your shell's configuration before setting any of the other environment variables!
+#     ⤷ The recommended value is: $HOME/.local/state
+# The $XDG_CACHE_HOME environment variable is not set, make sure to add it to your shell's configuration before setting any of the other environment variables!
+#     ⤷ The recommended value is: $HOME/.cache
+# The $XDG_RUNTIME_DIR environment variable is not set, make sure to add it to your shell's configuration before setting any of the other environment variables!
+#     ⤷ The recommended value is: /run/user/$UID
+
+XDG_DATA_HOME=$HOME/.local/share
+XDG_CONFIG_HOME=$HOME/.config
+XDG_STATE_HOME=$HOME/.local/state
+XDG_CACHE_HOME=$HOME/.cache
+XDG_RUNTIME_DIR=/run/user/$UID
+XDG_BIN_HOME=$HOME/.local/bin
